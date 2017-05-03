06c1feb9d56b9277b0174e39c18b1d0eee7f5af4
KursatCAKAL <kursat.cakal@hotmail.com>
2017-05-03 15:33:35 +0300

    Add files via upload

---
+++ ResponsiveLayout/Scripts/JavaScript.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $(".container").on("mouseover", '.social', function () {
+
+        $(this).closest('.social').addClass('bigger');
+        $(this).next('.social').addClass('medium');
+        $(this).next('.social').next('.social').addClass('smaller');
+        $(this).prev('.social').addClass('medium');
+        $(this).prev('.social').prev('.social').addClass('smaller');
+
+
+    });
+
+    $(".container").on("mouseout", '.social', function () {
+
+        $(this).closest('.social').removeClass('bigger');
+        $(this).next('.social').removeClass('medium');
+        $(this).next('.social').next('.social').removeClass('smaller');
+        $(this).prev('.social').removeClass('medium');
+        $(this).prev('.social').prev('.social').removeClass('smaller');
+
+    });
+
+});
